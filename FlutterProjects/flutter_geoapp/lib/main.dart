7ffa835120ad32c3d2387d02c01bde8aacb00248
geoffreylgv <geoffreylogovi2@gmail.com>
2023-01-27 19:18:02 +0000

    update add flutter test app

---
+++ FlutterProjects/flutter_geoapp/lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        fontFamily: 'Montsserat',
+      ),
+      themeMode: ThemeMode.light,
+      darkTheme: ThemeData.dark(),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        backgroundColor: Colors.blueAccent,
+        title: Text(
+          "JNA : Jeffrey New App",
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+    );
+  }
+}
